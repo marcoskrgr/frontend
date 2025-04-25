530687bb3b2d043b77aa2accf5e4650f2aa66085
AnthonyLoche <anthonylocheifc@gmail.com>
2025-04-25 20:14:57 -0300

    MEMORY-12 - Fazendo funcionalidades do jogo da memória

--- src/pages/Memory/index.jsx
+++ src/pages/Memory/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import GridMemory from "../../components/Memory/GridMemory";
+import HeaderMemory from "../../components/Memory/Title";
 
 function Memory(){
     return(
         <div>
-            <div>MEMORYA</div>
             <div className="grid-container">
+                <HeaderMemory taskName="Task 4" initialTime={180} score={0} gameStarted={true} />
                 <GridMemory />
             </div>
         </div>
